2517c92634fe7cfd4823f3fd90b4aa092d8f62d5
leo <923384777@qq.com>
2020-06-17 12:00:21 +0800

    init hooks

---
+++ hooks/index.jsx
@@ -0,0 +1,17 @@
+import useClippy from 'use-clippy'; //https://github.com/CharlesStover/use-clippy
+import { useWindowSize } from '@react-hook/window-size'; //https://github.com/jaredLunde/react-hook/tree/master/packages/window-size
+import { useMediaQuery, useMediaQueries } from '@react-hook/media-query'; https://github.com/jaredLunde/react-hook/tree/master/packages/media-query
+import { useAsync } from '@react-hook/async'; //https://github.com/jaredLunde/react-hook/tree/master/packages/async
+import useBrowserContextCommunication from 'react-window-communication-hook'; //https://github.com/AvraamMavridis/react-window-communication-hook
+import useScript from 'react-script-hook';  //https://github.com/hupe1980/react-script-hook
+
+
+export default {
+    useClippy,
+    useWindowSize,
+    useMediaQuery, 
+    useMediaQueries,
+    useAsync,
+    useBrowserContextCommunication,
+    useScript,
+}
